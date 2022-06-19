9083629a7e1228dd3d0605a976f5505a89bf484b
Jooko94 <88428511+JoacimLord@users.noreply.github.com>
2022-06-19 13:41:41 +0200

    Extracted the sorting algorithm demo to put in a seperate repo

--- Sandbox/src/Sandbox/Main.cpp
+++ Sandbox/src/Sandbox/Main.cpp
@@ -22,12 +22,6 @@ static int ParticleCounter = 0;
 static float Size = 35;
 static float force = 3.5f;
 
-static bool add = false;
-static bool sort = false;
-static int pillarCounter = 0;
-static int sortedAmount = 0;
-
-
 void Luna::Application::BuildUI()
 {
 	ImGui::Begin("ParticleStats");
@@ -41,15 +35,6 @@ void Luna::Application::BuildUI()
 		ImGui::SliderFloat("Size", &Size, 0, 50);
 		ImGui::ColorEdit4("StartColor", startColor);
 		ImGui::ColorEdit4("EndColor", endColor);
-
-	ImGui::End();
-
-	ImGui::Begin("Sorting Algos");
-
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::Text("Amount of pillars in scene %i", pillarCounter);
-		ImGui::Text("Sorted pillars %i", sortedAmount);
-		ImGui::Checkbox("Sort pillars", &sort);
 
 	ImGui::End();
 }
@@ -159,14 +144,6 @@ private:
 };
 
 
-struct Pillar
-{
-	glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
-	Luna::Anchor transform;
-	int value = 0;
-	bool hasBeenSorted = false;
-};
-
 int main()
 {
 	Luna::Application app("App");
@@ -177,21 +154,6 @@ int main()
 	float lastFrameTime = 0.0f;
 	glm::vec4 Black = { 0.0f, 0.0f, 0.0f, 1.0f };
 
-
-	//For bubble sort
-
-	glm::vec4 Red = { 1.0f, 0.0f, 0.0f,1.0f };
-	glm::vec4 Green = { 0.0f, 1.0f, 0.0f, 1.0f };
-	std::vector<Pillar> myRandomVector;
-	bool addedAllPillars = false;
-	bool sortedPillars = false;
-
-	bool hasBeenSorted = false;
-
-	float pillarTiming = 0.25f;
-	bool addNewPillar = true;
-	const int AMOUNT = 260; //260 for 5px wide
-
 	while (app.IsRunning())
 	{
 		float elapsedTime = app.GetElapsedRuntime();
@@ -199,128 +161,20 @@ int main()
 		lastFrameTime = elapsedTime;
 
 		ParticleCounter = particleSystem.GetParticleAmount();
-		pillarCounter = myRandomVector.size();
 
 		app.Clear(Black);
 
-		
 		//----------------------------------------------------
 		//					PARTICLE DEMO
 		//----------------------------------------------------
-		//{
-		//	particleSystem.SetParticleSpecification();
-		//	particleSystem.SetParticleScale();
-		//	particleSystem.SetParticleStartColor();
-		//	particleSystem.SetParticlePos();
-		//	particleSystem.UpdateAndEmitParticles(deltaTime);
-		//}
-
-
-
-		//----------------------------------------------------
-		//					BUBBLE SORT
-		//----------------------------------------------------
-		{	
-			//----------------------------------------------------
-			//					ADD RANDOM VALUES
-			//----------------------------------------------------
-			if (myRandomVector.size() < AMOUNT)
-			{
-				for (int i = 0; i < 1; i++)
-				{
-					Pillar pillar;
-
-					int random = std::rand() % 1000 + 1;
-
-					pillar.value = random;
-					myRandomVector.push_back(pillar);
-				}
-			}
-
-
-			//----------------------------------------------------
-			//					SORTING
-			//----------------------------------------------------	
-			if (sort && sortedAmount < myRandomVector.size())
-			{
-				for (int i = 0; i < 1; i++)
-				{
-					for (int j = 0; j < myRandomVector.size() - i -1; j++)
-					{
-
-						float tempA = myRandomVector[j].value;
-						float tempB = myRandomVector[j+1].value;
-						if (tempA > tempB)
-						{
-							myRandomVector[j] = myRandomVector[j + 1];
-							myRandomVector[j + 1].value = tempA;	
-
-							myRandomVector[j].color = Red;
-							myRandomVector[j + 1].color = Red;
-						}
-					}
-				}
-
-				sortedAmount++;
-
-				myRandomVector[myRandomVector.size() - sortedAmount].hasBeenSorted = true;
-				myRandomVector[myRandomVector.size() - sortedAmount].color = { Luna::Colors::Green };
-			}
+		{
+			particleSystem.SetParticleSpecification();
+			particleSystem.SetParticleScale();
+			particleSystem.SetParticleStartColor();
+			particleSystem.SetParticlePos();
+			particleSystem.UpdateAndEmitParticles(deltaTime);
 		}
 
-
-		//----------------------------------------------------
-		//					PRINT VALUES
-		//----------------------------------------------------
-		for (int i = 0; i < myRandomVector.size(); i++)
-		{
-			myRandomVector[i].transform.Scale.y = myRandomVector[i].value;
-			myRandomVector[i].transform.Scale.x = 5; //Works with 260 in amount
-
-			myRandomVector[i].transform.Translation.y = 0;
-			myRandomVector[i].transform.Translation.x = i * myRandomVector[i].transform.Scale.x;
-
-			//COLORS
-			//float transparency = 0;
-			//int tValue = std::rand() % 9;
-			//
-			//
-			//switch (tValue)
-			//{
-			//case 0:
-			//	transparency = 0.0f;
-			//	break;
-			//case 1:
-			//	transparency = 0.1f;
-			//	break;
-			//case 2:
-			//	transparency = 0.2f;
-			//	break;
-			//case 3:
-			//	transparency = 0.3f;
-			//	break;
-			//case 4:
-			//	transparency = 0.5f;
-			//	break;
-			//case 5:
-			//	transparency = 0.4f;
-			//	break;
-			//case 6:
-			//	transparency = 0.7f;
-			//	break;
-			//case 7:
-			//	transparency = 0.8f;
-			//	break;
-			//case 8:
-			//	transparency = 0.9f;
-			//	break;
-			//case 9:
-			//	transparency = 1.0f;
-			//	break;
-			//}
-
-			app.RenderShaderColor(myRandomVector[i].color, myRandomVector[i].transform.GetTransform());
-		}	
 		app.Display();
 	}
 }
